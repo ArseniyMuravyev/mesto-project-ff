c15c7a0edcbdac044a9eafb9658b62fe7403e729
ArseniyMuravyev <135731286+ArseniyMuravyev@users.noreply.github.com>
2023-12-01 12:24:25 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
-// @todo: Темплейт карточки
+function createCard(card, deleteCard) {
+  const cardTemplate = document.querySelector('#card-template').content
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true)
+  const cardImage = cardElement.querySelector('.card__image')
+  const cardTitle = cardElement.querySelector('.card__title')
+  const deleteButton = cardElement.querySelector('.card__delete-button')
 
-// @todo: DOM узлы
+  deleteButton.addEventListener('click', () => deleteCard(cardElement))
 
-// @todo: Функция создания карточки
+  cardImage.src = card.link
+  cardTitle.textContent = card.name
 
-// @todo: Функция удаления карточки
+  return cardElement
+}
 
-// @todo: Вывести карточки на страницу
+function renderCards(cards) {
+  const placesList = document.querySelector('.places__list')
+
+  cards.forEach(card => {
+    const cardElement = createCard(card, deleteCard)
+    placesList.append(cardElement)
+  })
+}
+
+const deleteCard = cardElement => cardElement.remove()
+
+renderCards(initialCards)
